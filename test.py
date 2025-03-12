0081503404fc23a2454ca388804c2add6ee071f9
Abhay Thakur <163429805+abhay41@users.noreply.github.com>
2025-03-12 11:23:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def second_Element(arr):
             
     return [second_max,second_smallest]
 
-arr=[5,3,7,2,3,4,2]
+arr=[5,3,7,3,4,15,20]
 second_max,second_smallest=second_Element(arr)
 print("Highest number is:",max(arr))
 print("second highest number is:",second_max)
