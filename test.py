a9b342189a19b1c634c24f52d4de12596c4bc131
abhay41 <tabhay0055@gmail.com>
2025-03-12 11:01:52 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+def second_Element(arr):
+    max=-float('inf')
+    for item in arr:
+        if item>max:
+            max=item
+    
+    second_max=-float('inf')
+    for item in arr:
+        if item>second_max and item<max:
+            second_max=item                        
+    
+    smallest=float('inf')
+    for item in arr:
+        if item<smallest:
+            smallest=item
+
+    second_smallest=float('inf')
+    for item in arr:
+        if item<second_smallest and item>smallest:
+            second_smallest=item
+            
+    return [second_max,second_smallest]
+
+arr=[5,3,7,2,3,4,2]
+second_max,second_smallest=second_Element(arr)
+print("Highest number is:",max(arr))
+print("second highest number is:",second_max)
+print("smallest number is:",min(arr))
+print("second smallest number is:",second_smallest)
